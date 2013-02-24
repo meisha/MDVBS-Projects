3ce1ebe6e43b314eb3b476d7b4da118445d32385
MRay <meisharay@gmail.com>
2013-02-24 18:07:31 -0500

    added more to webstuff
    can't run my app. It doesn't acknowledge it ugh

--- Java-1/VeganBrews/src/com/mray/lib/WebStuff.java
+++ Java-1/VeganBrews/src/com/mray/lib/WebStuff.java
@@ -1,8 +1,13 @@
 package com.mray.lib;
 
+import java.io.BufferedInputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.util.Log;
 
 public class WebStuff {
 
@@ -28,5 +33,26 @@ public class WebStuff {
 				_conn = true;
 			}
 		}
+	}
+	public static String getURLStringResponse(URL url){
+		String response = "";
+		
+		try{
+			URLConnection conn = url.openConnection();
+			BufferedInputStream bin = new BufferedInputStream(conn.getInputStream());
+			
+			byte[] contentBytes = new byte[1024];
+			int bytesRead = 0;
+			StringBuffer responseBuffer = new StringBuffer();
+			
+			while((bytesRead = bin.read(contentBytes)) != -1){
+				response = new String(contentBytes,0,bytesRead);
+				responseBuffer.append(response);
+			}
+			return responseBuffer.toString();
+		} catch(Exception e){
+			Log.e("URL RESPONSE ERROR", "getURLStringResponse");
+		}
+		return response;
 	}
 }
