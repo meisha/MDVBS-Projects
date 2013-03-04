2b18a8f3457871848418ee98523067cf8a126027
MRay <meisharay@gmail.com>
2013-03-04 17:32:59 -0500

    set up my activity for java 2

---
+++ Java 2/ActivityPractice/src/com/mray/activitypractice/MainActivity.java
@@ -0,0 +1,37 @@
+package com.mray.activitypractice;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		Button next = (Button) findViewById(R.id.next_button);
+		next.setOnClickListener(new OnClickListener(){
+			
+			@Override
+			public void onClick(View v){
+				Intent myIntent = new Intent(v.getContext(), NextActivity.class);
+				v.getContext().startActivity(myIntent);
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
