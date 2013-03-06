1209b163e152b084fd9d9c5f392d7afddd647a02
MRay <meisharay@gmail.com>
2013-03-06 17:44:30 -0500

    web view working

--- Java 2/ForgetMeNot/src/com/mray/forgetmenot/DisplayMessageActivity.java
+++ Java 2/ForgetMeNot/src/com/mray/forgetmenot/DisplayMessageActivity.java
@@ -12,19 +12,26 @@ package com.mray.forgetmenot;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
 
 // TODO: Auto-generated Javadoc
 /**
  * The Class DisplayMessageActivity.
  */
-public class DisplayMessageActivity extends Activity {
+public class DisplayMessageActivity extends Activity implements OnClickListener {
+	
+	Button myBlog_intent;
+	
 	@Override
 	public void onCreate(Bundle bundle) {
 		super.onCreate(bundle);
 		setContentView(R.layout.second);
+		myBlog_intent = (Button) findViewById(R.id.button1);
 
 		Bundle extras = getIntent().getExtras();
 		if (extras == null) return;
@@ -36,13 +43,24 @@ public class DisplayMessageActivity extends Activity {
 			EditText text2 = (EditText) findViewById(R.id.EditText02);
 			text1.setText(value1);
 			text2.setText(value2+"");
-		}
+			
+			myBlog_intent.setOnClickListener(new OnClickListener(){
+			// Implicit Intent Blog Website
+			@Override
+			public void onClick(View blogs){
+				Uri myURL = Uri.parse("http://dsc.discovery.com/tv-shows/curiosity/topics/10-ways-to-improve-memory.htm");
+				Intent myBlog_intent = new Intent(Intent.ACTION_VIEW, myURL);
+				startActivity(myBlog_intent);
+			}
+		});
 	}
-
+	}
+	
 	public void onClick(View view) {
 		finish();
 	}
-
+	
+	
 	@Override
 	public void finish() {
 		Intent data = new Intent();
