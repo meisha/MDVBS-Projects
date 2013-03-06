cf1a05226af82f39a61253f48a7f3e4b232bd1ea
MRay <meisharay@gmail.com>
2013-03-06 08:08:12 -0500

    Added in some comments about what the app is about

--- Java 2/ForgetMeNot/gen/com/mray/forgetmenot/R.java
+++ Java 2/ForgetMeNot/gen/com/mray/forgetmenot/R.java
@@ -42,6 +42,7 @@ public final class R {
         public static final int hello_world=0x7f050002;
         public static final int save_btn=0x7f050004;
         public static final int text1=0x7f050005;
+        public static final int title_main_activty=0x7f050006;
     }
     public static final class style {
         /** 
--- Java 2/ForgetMeNot/src/com/mray/forgetmenot/MainActivity.java
+++ Java 2/ForgetMeNot/src/com/mray/forgetmenot/MainActivity.java
@@ -1,17 +1,55 @@
+/*
+ * project	ForgetMeNot
+ * 
+ * package 	com.mray.forgetmenot
+ * 
+ * @author	Meisha Ray
+ * 
+ * date		Mar 6, 2013
+ */
 package com.mray.forgetmenot;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class MainActivity.
+ * This is a memory saving app. 
+ * If the user has something that they don't want to forget, they
+ * can make a quick note of it in this app and refer to it later on.
+ * At some point I'd like to add alerts to remind the user to look at the app
+ * to see what they may not be remebering.
+ */
 public class MainActivity extends Activity {
+	
+	/**
+	 * Send message.
+	 *
+	 * @param view the view
+	 * 
+	 * Adding my intent for the save button.
+	 * This button will take the user to the next activity when pressed.
+	 */
+	public void sendMessage(View view){
+		Intent intent = new Intent(this, DisplayMessageActivity.class);
+	}
 
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
 
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
+	 */
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
