a899b97f27e4319b10d60458c351a6891d55cfac
MRay <meisharay@gmail.com>
2013-03-12 08:34:08 -0400

    added another xml file

--- Java 2/ForgetMeNot/gen/com/mray/forgetmenot/R.java
+++ Java 2/ForgetMeNot/gen/com/mray/forgetmenot/R.java
@@ -38,9 +38,10 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int fragment_main=0x7f030001;
-        public static final int fragment_sec=0x7f030002;
-        public static final int second=0x7f030003;
+        public static final int activity_master=0x7f030001;
+        public static final int fragment_main=0x7f030002;
+        public static final int fragment_sec=0x7f030003;
+        public static final int second=0x7f030004;
     }
     public static final class menu {
         public static final int display_message=0x7f070000;
