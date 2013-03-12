a3b4fed495ad741176fa81bd6b475c48e339f3c4
MRay <meisharay@gmail.com>
2013-03-12 10:59:29 -0400

    Working on the fragments

---
+++ Java 2/JavaClassApp/src/com/mray/justanapp/MainFragment.java
@@ -0,0 +1,46 @@
+package com.mray.justanapp;
+
+
+import android.annotation.SuppressLint;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+@SuppressLint("NewApi")
+public class MainFragment extends Fragment implements OnClickListener {
+	
+	// Global Vars
+	private TextView foodText;
+	private EditText edit;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		View view = inflater.inflate(R.layout.fragment_main, container);
+		
+		edit = (EditText) view.findViewById(R.id.EditText01);
+		
+		Button button = (Button) view.findViewById(R.id.button01);
+		button.setOnClickListener(this);
+		
+		foodText = (TextView) view.findViewById(R.id.textView02);
+		
+		return view;
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
